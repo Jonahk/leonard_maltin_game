5a9f29f57c5d5d6e4251f157e9657c47c39bcf3b
Jonah <jk51287@gmail.com>
2013-12-24 11:06:22 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+LeonardMaltinGame::Application.config.secret_key_base = 'd071232e736f8f349b2c21de121e23815cc0fa1236d4dc6f46a7850b1aa800ee6ea92b54b9213806e8d0d5764afa1f7dfa11c1b1c2478fdf79c0de5bf1b17c5b'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+LeonardMaltinGame::Application.config.session_store :cookie_store, key: '_leonard_maltin_game_session'
